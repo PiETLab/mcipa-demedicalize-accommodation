11136c9a04c78690070a5fe3a04d14aed6a349e2
Melanie Baljko <35408034+mbaljko@users.noreply.github.com>
2026-07-18 20:28:52 -0400

    fix: improve explorer active-link contrast

--- quartz.config.ts
+++ quartz.config.ts
@@ -200,7 +200,7 @@ const config: QuartzConfig = {
           darkgray: "#4e4e4e",
           dark: "#2b2b2b",
           secondary: "#284b63",
-          tertiary: "#84a98c",
+          tertiary: "#3e7a4c",
           highlight: "rgba(143, 159, 169, 0.15)",
           textHighlight: "#fff23688",
         },
